10030ff47c400c4607084233915d976cabd2fefc
AJIEKCanderG <sanya.spb_vohta@mail.ru>
2021-03-05 12:31:40 +0300

    0. Stream API [#353117] refactor

--- src/main/java/ru/job4j/stream/Array.java
+++ src/main/java/ru/job4j/stream/Array.java
@@ -1,19 +1,13 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Array {
     public static void main(String[] args) {
-        List<Integer> start = new ArrayList<>(List.of(-2, -1, 0, 1, 2));
-        System.out.println("Заполнил ArrayList числами: ");
-        start.forEach(System.out::print);
-        System.out.println();
-        System.out.println("Получил поток положительных чисел: ");
-        List<Integer> finish = start.stream()
-                .filter(list -> list >= 0)
+        List<Integer> start = List.of(-2, -1, 0, 1, 2).stream()
+                .filter(n -> n > 0)
                 .collect(Collectors.toList());
-        finish.forEach(System.out::print);
+        start.forEach(System.out::print);
     }
 }
